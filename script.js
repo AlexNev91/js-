72d751708c51f5c196f44bad1a89fbb48b06221c
Alex <alexx00000007gmail.com>
2024-07-13 12:29:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+//const resault = confirm('are  you');
+//console log. (resault);
+
+//const answer = prompt('вам есть 18', '18');
+//console. log(answer)
+//console. log(typeof(answer));
+
+const answer = []
+
+/* answer [0] = prompt('Скоько вам лет ?','');
+answer [1] = prompt('как ваше  имя  ?','');
+answer [2] = prompt('как ваше  фамилия  ?',''); */
+
+document.write(typeof(answers));
